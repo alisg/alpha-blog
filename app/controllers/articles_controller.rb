4334cb22619abc9d2954df14ffa67f11f92d3471
Alice <alisagrigoryan1@gmail.com>
2022-09-30 21:12:10 +0000

    Adding a new article and warning when title and description doesn't align with validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,19 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+    
+    def new
+        @article = Article.new
+    end
+    
+    def create
+        @article = Article.new(params.require(:article).permit(:title,:description))
+        if @article.save
+            flash[:notice] = 'The Article is successfully created' #throwing an alert and you can use tthis from html.erb files 
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
 end
