a12040a0227598eef09857ab6271fbe0afa9e63e
Alice <alisagrigoryan1@gmail.com>
2022-11-06 22:17:17 +0000

    styling with bootstrap

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: {minimum: 6, maxiimum: 100}
-    validates :description, presence: true, length: {minimum: 10, maxiimum: 300}
+    validates :description, presence: true, length: {minimum: 10, maxiimum: 1000}
 end
