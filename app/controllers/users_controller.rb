cece4cbd498b43d5340bd219e69d85877805a244
alisa.grigoryan <alisa.grigoryan@vineti.com>
2023-01-16 01:22:12 +0400

    final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            flash[:notice] = "Welcome to Alpha blog #{@user.username}"
+            flash[:notice] = "Welcome to Cool Bloggers #{@user.username}"
             redirect_to articles_path
         else
             render 'new'
